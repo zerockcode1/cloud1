1992c3ca4b66bc6e615a99f7a989229438ca0de9
zkang507 <zkang507@gmail.com>
2022-02-12 00:53:08 +0900

    Initial commit

---
+++ src/main/java/org/zerock/cloud1/Cloud1Application.java
@@ -0,0 +1,13 @@
+package org.zerock.cloud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloud1Application.class, args);
+    }
+
+}
